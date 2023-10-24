04485069165d19f52f42106067b3e05385225981
Dennis Burger <dennis@dutchwebworks.nl>
2023-10-24 19:49:10 +0200

    Updated with more demos

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         "sans": ["ui-sans-serif", "system-ui"],
       },
+      boxShadow: {
+        p80: "0 10px 5px theme('colors.p80-primary'), 0 0 20px theme('colors.p80-secondary')"
+      },
       colors: {
         "p80-primary": "#3da8f5",
         "p80-secondary": "#2a526f",
